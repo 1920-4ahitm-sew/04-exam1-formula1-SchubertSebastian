1388cfb94ee2299eec74d549646a182f33395712
you <you@example.com>
2019-11-15 16:23:42 +0100

    added table annotation in Team

--- src/main/java/at/htl/formula1/entity/Team.java
+++ src/main/java/at/htl/formula1/entity/Team.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
  * The id's are assigned by the database.
  */
 @Entity
+@Table(name = "F1_TEAM")
 public class Team {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
